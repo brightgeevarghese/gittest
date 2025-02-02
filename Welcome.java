4ec179b45761f14af988f40feecf35e58006fdf7
Bright Gee Varghese <bvarghese@miu.edu>
2025-02-02 16:20:00 -0600

    Deals text is modified

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,6 @@
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to my app!");
-        System.out.println("Today's deals are...");
+        System.out.println("Today's exciting deals are...");
     }
 }
